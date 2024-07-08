a77b7655357bf55ce49bd5a57ebc3467ccebdc03
JVCRJA <jorgevalenciacalloapaza@gmail.com>
2024-07-08 15:34:17 -0400

    corregido

--- database/migrations/2024_06_22_021817_create_cajeros_table.php
+++ database/migrations/2024_06_22_021817_create_cajeros_table.php
@@ -6,28 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('cajeros', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('confirmation_token')->nullable(); // Campo para el token de confirmación
-            $table->foreignId('sucursale_id')->nullable()->constrained('sucursales');
-            $table->integer('estado')->default(1);
-            $table->timestamps();
-         });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create('cajeros', function (Blueprint $table) {
+         $table->id();
+         $table->string('name');
+         $table->string('email')->unique();
+         $table->string('password');
+         $table->integer('codigo_confirmacion')->nullable();
+         $table->string('confirmation_token')->nullable(); // Campo para el token de confirmación
+         $table->foreignId('sucursale_id')->nullable()->constrained('sucursales');
+         $table->integer('estado')->default(1);
+         $table->string('role');
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('cajeros');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists('cajeros');
+   }
 };
