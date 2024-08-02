bc58d76ff73ba7321529b6063380e1066aa3faa1
JVCRJA <jorgevalenciacalloapaza@gmail.com>
2024-08-02 10:26:27 -0400

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CajeroController;
 
-
-Route::middleware(['jwt.auth'])->group(function () {
+Route::middleware(['jwt.auth', 'working_hours'])->group(function () {
    Route::apiResource('/empresa', 'EmpresaController');
    Route::apiResource('/sucursales', 'SucursaleController');
    Route::apiResource('/cajeros', 'CajeroController');
@@ -12,15 +11,22 @@ Route::middleware(['jwt.auth'])->group(function () {
    Route::apiResource('/servicios', 'ServicioController');
    Route::apiResource('/ventas', 'VentaController');
    Route::apiResource('/notificaciones', 'NotificacioneController');
-   Route::get('venta/pdf/{codigoSeguimiento}', 'VentaController@getPdfUrl');
+
    Route::get('ventas/consultar/{codigoSeguimiento}', 'VentaController@consultarVenta');
    Route::patch('ventas/anular/{cuf}', 'VentaController@anularFactura');
    Route::post('venta2', 'VentaController@venta2');
 
-   Route::put('cajeros/activar/{id}', [CajeroController::class, 'activar']);
+   Route::get('/special-access-logs', 'CajeroController@listSpecialAccessLogs');
 });
 Route::post('login', 'CajeroController@login');
 Route::post('verificar-codigo-confirmacion', 'CajeroController@verificarCodigoConfirmacion');
 Route::post('request-password-reset', [CajeroController::class, 'requestPasswordReset']);
 Route::post('reset-password/{token}', [CajeroController::class, 'resetPassword']);
 Route::post('/cajeros/confirmar/{token}', [CajeroController::class, 'confirmar'])->name('cajeros.confirmar');
+
+use App\Http\Controllers\VentaController;
+
+Route::get('/ventas/dia/{cajeroId}', [VentaController::class, 'ventasDelDia']);
+Route::get('/ventas/mes/{cajeroId}', [VentaController::class, 'ventasDelMes']);
+Route::post('/ventas/fecha/{cajeroId}', [VentaController::class, 'ventasPorFecha']);
+Route::get('venta/pdf/{codigoSeguimiento}', 'VentaController@getPdfUrl');
