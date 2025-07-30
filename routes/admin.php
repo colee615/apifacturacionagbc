da259767da0eee3d103e5f9b05a77df15999d181
JVCRJA <jorgevalenciacalloapaza@gmail.com>
2025-07-30 15:06:38 -0400

    QUITAR RUTAS DE PROTECCION DEVOLVER CAMBIOS

--- routes/admin.php
+++ routes/admin.php
@@ -4,17 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CajeroController;
 
 Route::middleware(['jwt.auth', 'working_hours'])->group(function () {
-   Route::apiResource('/empresa', 'EmpresaController');
-   Route::apiResource('/sucursales', 'SucursaleController');
-   Route::apiResource('/cajeros', 'CajeroController');
-   Route::apiResource('/clientes', 'ClienteController');
-   Route::apiResource('/servicios', 'ServicioController');
-   Route::apiResource('/ventas', 'VentaController');
-   Route::apiResource('/notificaciones', 'NotificacioneController');
+   // Route::apiResource('/empresa', 'EmpresaController');
+   // Route::apiResource('/sucursales', 'SucursaleController');
+   // Route::apiResource('/cajeros', 'CajeroController');
+   // Route::apiResource('/clientes', 'ClienteController');
+   // Route::apiResource('/servicios', 'ServicioController');
+   // Route::apiResource('/ventas', 'VentaController');
+   // Route::apiResource('/notificaciones', 'NotificacioneController');
 
-   Route::get('ventas/consultar/{codigoSeguimiento}', 'VentaController@consultarVenta');
-   Route::patch('ventas/anular/{cuf}', 'VentaController@anularFactura');
-   Route::post('venta2', 'VentaController@venta2');
+   // Route::get('ventas/consultar/{codigoSeguimiento}', 'VentaController@consultarVenta');
+   // Route::patch('ventas/anular/{cuf}', 'VentaController@anularFactura');
+   // Route::post('venta2', 'VentaController@venta2');
 });
 Route::post('login', 'CajeroController@login');
 Route::post('verificar-codigo-confirmacion', 'CajeroController@verificarCodigoConfirmacion');
@@ -30,3 +30,14 @@ Route::post('/ventas/fecha/{cajeroId}', [VentaController::class, 'ventasPorFecha
 Route::get('venta/pdf/{codigoSeguimiento}', 'VentaController@getPdfUrl');
 
 Route::get('/special-access-logs', 'CajeroController@listSpecialAccessLogs');
+Route::apiResource('/empresa', 'EmpresaController');
+Route::apiResource('/sucursales', 'SucursaleController');
+Route::apiResource('/cajeros', 'CajeroController');
+Route::apiResource('/clientes', 'ClienteController');
+Route::apiResource('/servicios', 'ServicioController');
+Route::apiResource('/ventas', 'VentaController');
+Route::apiResource('/notificaciones', 'NotificacioneController');
+
+Route::get('ventas/consultar/{codigoSeguimiento}', 'VentaController@consultarVenta');
+Route::patch('ventas/anular/{cuf}', 'VentaController@anularFactura');
+Route::post('venta2', 'VentaController@venta2');
