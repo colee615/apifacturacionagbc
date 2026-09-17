ab54d202bf549fc2320fa60374efc1fc59d68f7f
colee615 <94701865+colee615@users.noreply.github.com>
2026-09-17 13:37:04 -0400

    Revert "Cambiios"
    This reverts commit 3d3e191058e3de44e3ce196890ceb5368595c17b.

--- app/Http/Controllers/FacturaVentaApiController.php
+++ app/Http/Controllers/FacturaVentaApiController.php
@@ -824,12 +824,6 @@ class FacturaVentaApiController extends Controller
                 };
             }
 
-            // Una notificacion OBSERVADO/CREADO tardia no puede reemplazar
-            // una emision que ya fue confirmada como PROCESADA.
-            if ($currentStatus === 'PROCESADA' && $notificacion->estado !== 'EXITO') {
-                return 'PROCESADA';
-            }
-
             return match ($notificacion->estado) {
                 'EXITO' => 'PROCESADA',
                 'OBSERVADO' => 'OBSERVADA',
